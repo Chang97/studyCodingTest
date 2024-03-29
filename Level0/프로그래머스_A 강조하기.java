8259542458225922aaf66ff5357ce9a9b546df2a
Chang97 <ckdrbsaos@naver.com>
2024-03-29 16:28:06 +0900

    20240329_기초문제

--- Level0/프로그래머스_A 강조하기.java
+++ Level0/프로그래머스_A 강조하기.java
@@ -1,5 +1,11 @@
 class Solution {
     public String solution(String myString) {
-        return myString.toUpperCase();
+        char[] arr = myString.toLowerCase().toCharArray();
+        for (int i = 0; i < arr.length; i++) {
+            if (Character.compare(arr[i], 'a') == 0) {
+                arr[i] = 'A';
+            }
+        }
+        return String.valueOf(arr);
     }
 }
