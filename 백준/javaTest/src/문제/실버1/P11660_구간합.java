f0286d0282c97ddc07dfcb0719eaf4a2c726d8f8
김창균 <ckdrbsaos@naver.com>
2024-04-04 22:06:45 +0900

    2차원배열 구간합

--- 백준/javaTest/src/문제/실버1/P11660_구간합.java
+++ 백준/javaTest/src/문제/실버1/P11660_구간합.java
@@ -5,6 +5,9 @@ package 문제.실버1;
  * 2. 배열 값을 입력받는다.
  * 3. xStart, xEnd, yStart, tEnd 를 M의 개수만큼 입력 받는다.
  * 4. 구간합을 출력한다.
+ * 추후에 다시 풀어볼 필요 있음
+ * 공식 산출에 어려움이 있음... 그냥 외워야되나..
+ * 1차원 배열 공식과 크게 다를게 없긴 함
  */
 import java.io.*;
 import java.util.*;
@@ -33,14 +36,14 @@ public class P11660_구간합 {
         S[i][j] = S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1] + numArr[i][j] ;
       }
     }
-
-    for (int i = 0; i <= M; i++) {
+    for (int i = 0; i < M; i++) {
       st = new StringTokenizer(rb.readLine());
       int x1 = Integer.parseInt(st.nextToken());
       int y1 = Integer.parseInt(st.nextToken());
       int x2 = Integer.parseInt(st.nextToken());
       int y2 = Integer.parseInt(st.nextToken());
-      
+      long result = S[x2][y2] - S[x2][y1 - 1] - S[x1 - 1][y2] + S[x1 - 1][y1 - 1];
+      System.out.println(result);
     }
 
 
