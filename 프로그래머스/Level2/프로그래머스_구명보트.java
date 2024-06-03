cf4d20d4b92fd228643d90fbd6ea9908a037a39c
김창균 <ckdrbsaos@naver.com>
2024-06-03 23:01:33 +0900

    20240603_프로그래머스_Level2

---
+++ 프로그래머스/Level2/프로그래머스_구명보트.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] people, int limit) {
+        Arrays.sort(people);
+        int s = 0;
+        int e = people.length - 1;
+        int answer = 0;
+        
+        while (s <= e) {
+            if (people[s] + people[e] <= limit) {
+                s++;
+            }
+            e--;
+            answer++;
+        }
+        
+        return answer;
+    }
+}
