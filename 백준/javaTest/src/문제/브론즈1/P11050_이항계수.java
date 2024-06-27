b39f03a9aeea2a06b876715421247c4486b37c45
김창균 <ckdrbsaos@naver.com>
2024-06-27 21:02:37 +0900

    20240627_백준_조합

---
+++ 백준/javaTest/src/문제/브론즈1/P11050_이항계수.java
@@ -0,0 +1,27 @@
+package 문제.브론즈1;
+
+import java.util.Scanner;
+
+public class P11050_이항계수 {
+  static int N, K;
+  static int[][] D;
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    N = sc.nextInt();
+    K = sc.nextInt();
+    D = new int[N + 1][N + 1];
+    for (int i = 0; i <= N; i++) {
+      D[i][1] = i;
+      D[i][0] = 1;
+      D[i][i] = 1;
+    }
+
+    for (int i = 2; i <= N; i++) {
+      for (int j = 1; j < i; j++) {
+        D[i][j] = D[i - 1][j] + D[i - 1][j - 1];
+      }
+    }
+    System.out.println(D[N][K]);
+    sc.close();
+  }
+}
