bc82b181d8f4b81b9f148745f951260e7d42d9c8
Chang97 <ckdrbsaos@naver.com>
2025-04-28 15:19:44 +0900

    조약돌 꺼내기

--- 백준/javaTest/src/혼자풀기2/P13251.java
+++ 백준/javaTest/src/혼자풀기2/P13251.java
@@ -5,6 +5,29 @@ import java.util.*;
 
 public class P13251 {
     public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int M = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] items = new int[M];
+        int T = 0;
+        for (int i = 0; i < M; i++) {
+            items[i] = Integer.parseInt(st.nextToken());
+            T += items[i];
+        }
+        int K = Integer.parseInt(br.readLine());
         
+        double ans = 0.0;
+        double[] prob = new double[M];
+        for (int i = 0; i < M; i++) {
+            if (items[i] >= K) {
+                prob[i] = 1.0;
+                for (int k = 0; k < K; k++) {
+                    prob[i] *= (double) (items[i] - k) / (T - k);
+                }
+                ans += prob[i];
+            }
+        }
+        System.out.println(ans);
     }
 }
