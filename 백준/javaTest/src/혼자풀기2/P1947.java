0117d2413f52525380870c4f13fd1c77c6b793da
Chang97 <ckdrbsaos@naver.com>
2025-04-29 16:31:08 +0900

    선물전달

---
+++ 백준/javaTest/src/혼자풀기2/P1947.java
@@ -0,0 +1,20 @@
+package 혼자풀기2;
+
+import java.util.*;
+import java.io.*;
+
+public class P1947 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        int N = Integer.parseInt(br.readLine());
+        long mod = 1000000000;
+        long[] D = new long[N + 1];
+        D[1] = 0;
+        D[2] = 1;
+        for (int i = 3; i <= N; i++) {
+            D[i] = (i - 1) * (D[i - 2] + D[i - 1]) % mod;
+        }
+        System.out.println(D[N]);
+    }
+}
