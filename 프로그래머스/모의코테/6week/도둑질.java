d74f3bc56ee4c7bb65054e8f448e9a2f41472246
Chang97 <ckdrbsaos@naver.com>
2026-01-14 22:35:00 +0900

    6주차 (모의코테 D)

---
+++ 프로그래머스/모의코테/6week/도둑질.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int solution(int[] money) {
+        int answer = 0;
+        int len = money.length;
+        if (len == 1) return money[0];
+        int[] dp1 = new int[len];
+        dp1[0] = money[0];
+        dp1[1] = Math.max(money[0], money[1]);
+        for (int i = 2; i < len - 1; i++) {
+            dp1[i] = Math.max(dp1[i - 1], dp1[i - 2] + money[i]);
+        }
+        
+        int[] dp2 = new int[len];
+        dp2[1] = money[1];
+        for (int i = 2; i < len; i++) {
+            dp2[i] = Math.max(dp2[i - 1], dp2[i - 2] + money[i]);
+        }
+        return Math.max(dp1[len - 2], dp2[len - 1]);
+    }
+}
---
+++ 프로그래머스/모의코테/6week/등굣길.java
@@ -0,0 +1,27 @@
+import java.util.*;
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int[][] dp = new int[n + 1][m + 1];
+        boolean[][] water = new boolean[n + 1][m + 1];
+        int MOD = 1000000007;
+        
+        for (int[] puddle : puddles) {
+            water[puddle[1]][puddle[0]] = true;
+        }
+        if (!water[1][1]) dp[1][1] = 1;
+        
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (water[i][j]) {
+                    dp[i][j] = 0;
+                    continue;
+                } 
+                if (i == 1 && j == 1) continue;
+                int a = i > 1 ? dp[i - 1][j] : 0;
+                int b = j > 1 ? dp[i][j - 1] : 0;
+                dp[i][j] = (a + b) % MOD;
+            }
+        }
+        return dp[n][m];
+    }
+}
